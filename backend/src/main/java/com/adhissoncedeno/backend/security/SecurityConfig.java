bac3c50ebf14419695bccb62ecd39e52034b73d0
Adhisson Cedeño <adhisson89@hotmail.com>
2025-03-16 16:40:36 -0500

    feat (backend): enhance security configuration to support CORS and permit OPTIONS requests

--- backend/src/main/java/com/adhissoncedeno/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/adhissoncedeno/backend/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.adhissoncedeno.backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,7 +29,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS ,"/**").permitAll()
                 .requestMatchers("/graphql").permitAll()
                 .requestMatchers("/graphiql").permitAll()
                 .requestMatchers("/auth/login").permitAll()
